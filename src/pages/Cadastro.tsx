e98fec2bef0a0dc85de14caa59a24532821255d5
Guilherme Ferreira <guigododoti@gmail.com>
2025-07-16 13:41:56 -0300

    .
    .

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -253,7 +253,7 @@ useEffect(() => {
   } finally {
     setIsLoading(false);
   }
-};
+}; 
   return (
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="container mx-auto px-4">
